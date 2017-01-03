e55705f4c054941f8309f05bfdfccf58d98289ba
Adam Tahoun <ataho001@fiu.edu>
2017-01-02 19:59:42 -0500

    completed 80% of week 6

--- coursera/ex5/linearRegCostFunction.m
+++ coursera/ex5/linearRegCostFunction.m
@@ -1,19 +1,19 @@
 function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
-%LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear 
+%LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear
 %regression with multiple variables
-%   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
-%   cost of using theta as the parameter for linear regression to fit the 
+%   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the
+%   cost of using theta as the parameter for linear regression to fit the
 %   data points in X and y. Returns the cost in J and the gradient in grad
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost and gradient of regularized linear 
+% Instructions: Compute the cost and gradient of regularized linear
 %               regression for a particular choice of theta.
 %
 %               You should set J to the cost and grad to the gradient.
@@ -22,9 +22,19 @@ grad = zeros(size(theta));
 
 
 
+pred = X * theta;
+err = pred - y;
+sqrderr = err .^2;
+norm = (lambda / (2 * m)) * sum(theta(2:length(theta),:) .^ 2);
 
 
+J = (1/(2*m)) * sum(sqrderr) + norm;
 
+thetaExcludingZero = theta;
+thetaExcludingZero(1) = 0;
+reg_sum = (lambda / m) * thetaExcludingZero';
+
+grad = sum(err.*X)./m + reg_sum;
 
 
 
--- coursera/ex5/polyFeatures.m
+++ coursera/ex5/polyFeatures.m
@@ -10,12 +10,16 @@ function [X_poly] = polyFeatures(X, p)
 X_poly = zeros(numel(X), p);
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: Given a vector X, return a matrix X_poly where the p-th 
+% Instructions: Given a vector X, return a matrix X_poly where the p-th
 %               column of X contains the values of X to the p-th power.
 %
-% 
+%
+X_poly = X
+for i = 2:p
 
+  X_hold = X .^i;
 
+  X_poly = [X_poly X_hold];
 
 
 
