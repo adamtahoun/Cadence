50d7eec47ec9e23901f9fdc63f8a9501feb2988b
Adam Tahoun <ataho001@fiu.edu>
2017-01-27 11:19:09 -0500

    completed ex1_multi for presentation

--- coursera/ex1/ex1_multi.m
+++ coursera/ex1/ex1_multi.m
@@ -3,11 +3,11 @@
 %
 %  Instructions
 %  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear regression exercise. 
 %
-%  You will need to complete the following functions in this 
+%  This file contains code that helps you get started on the
+%  linear regression exercise.
+%
+%  You will need to complete the following functions in this
 %  exericse:
 %
 %     warmUpExercise.m
@@ -29,12 +29,12 @@
 %% ================ Part 1: Feature Normalization ================
 
 %% Clear and Close Figures
-clear ; close all; clc
+clear all; close all; clc
 
 fprintf('Loading data ...\n');
 
 %% Load Data
-data = load('ex1data2.txt');
+data = csvread('ex1data2.txt');
 X = data(:, 1:2);
 y = data(:, 3);
 m = length(y);
@@ -60,13 +60,13 @@ X = [ones(m, 1) X];
 % ====================== YOUR CODE HERE ======================
 % Instructions: We have provided you with the following starter
 %               code that runs gradient descent with a particular
-%               learning rate (alpha). 
+%               learning rate (alpha).
 %
-%               Your task is to first make sure that your functions - 
-%               computeCost and gradientDescent already work with 
+%               Your task is to first make sure that your functions -
+%               computeCost and gradientDescent already work with
 %               this starter code and support multiple variables.
 %
-%               After that, try running gradient descent with 
+%               After that, try running gradient descent with
 %               different values of alpha and see which one gives
 %               you the best result.
 %
@@ -82,10 +82,10 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
-num_iters = 400;
+alpha = 1; % modified from 0.01 because 3.2.1
+num_iters = 50; %modified from 100 because 3.2.1
 
-% Init Theta and Run Gradient Descent 
+% Init Theta and Run Gradient Descent
 theta = zeros(3, 1);
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
 
@@ -100,17 +100,67 @@ fprintf('Theta computed from gradient descent: \n');
 fprintf(' %f \n', theta);
 fprintf('\n');
 
+
+% selecting learning rates.
+% 1 is best.
+% hold on;
+% theta = zeros(3, 1);
+% [theta,  J_history] = gradientDescentMulti(X, y,  theta,  alpha/10.0,  num_iters);
+% plot(1:numel(J_history), J_history, '-y', 'LineWidth', 2);
+% fprintf('Theta computed from gradient descent: \n');
+% fprintf(' %f \n', theta);
+% fprintf('\n');
+
+
+% theta = zeros(3, 1);
+% [theta,  J_history] = gradientDescentMulti(X, y,  theta,  alpha/100.0,  num_iters);
+% plot(1:numel(J_history), J_history, '-r', 'LineWidth', 2);
+% fprintf('Theta computed from gradient descent: \n');
+% fprintf(' %f \n', theta);
+% fprintf('\n');
+
+
+% theta = zeros(3, 1);
+% [theta,  J_history] = gradientDescentMulti(X, y,  theta,  alpha/1000.0,  num_iters);
+% plot(1:numel(J_history), J_history, '-k', 'LineWidth', 2);
+% fprintf('Theta computed from gradient descent: \n');
+% fprintf(' %f \n', theta);
+% fprintf('\n');
+
+
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
-
-
+price = [1 (1650-mu(1))/sigma(1) (3-mu(2))/sigma(2)]*theta; % You should change this
 % ============================================================
 
 fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
          '(using gradient descent):\n $%f\n'], price);
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+% Plotting Training and regressioned data.
+fprintf('Plotting Training and regressioned results by gradient descent.\n');
+X = [ones(m, 1) data(:, 1:2)]; %denormalize features
+figure;
+plot3(X(:,2),X(:,3),y,"o");
+xlabel('sq-ft of room');
+ylabel('#bedroom');
+zlabel('price');
+grid;
+hold on;
+xx = linspace(0,5000,25);
+yy = linspace(1,5,25);
+zz = zeros(size(xx,2),size(yy,2));
+for i=1:size(xx,2)
+for j=1:size(yy,2)
+  zz(i,j) = [1 (xx(i)-mu(1))/sigma(1) (yy(j)-mu(2))/sigma(2)]*theta;
+end
+end
+mesh(xx,yy,zz);
+title('Result of Gradient Descent');
+legend('Training data', 'Linear regression');
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
@@ -120,12 +170,12 @@ pause;
 fprintf('Solving with normal equations...\n');
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: The following code computes the closed form 
+% Instructions: The following code computes the closed form
 %               solution for linear regression using the normal
-%               equations. You should complete the code in 
+%               equations. You should complete the code in
 %               normalEqn.m
 %
-%               After doing so, you should complete this code 
+%               After doing so, you should complete this code
 %               to predict the price of a 1650 sq-ft, 3 br house.
 %
 
@@ -149,11 +199,32 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
-
-
+price = [1 1650 3]*theta; % You should change this
 % ============================================================
 
 fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
          '(using normal equations):\n $%f\n'], price);
+fprintf('Program paused. Press enter to continue.\n');
+pause;
 
+
+% Plotting Training and regressioned data.
+fprintf('Plotting Training and regressioned results by solving normal equation.\n');
+figure;
+plot3(X(:,2),X(:,3),y,"o");
+xlabel('sq-ft of room');
+ylabel('#bedroom');
+zlabel('price');
+grid;
+hold on;
+xx = linspace(0,5000,25);
+yy = linspace(1,5,25);
+zz = zeros(size(xx,2),size(yy,2));
+for i=1:size(xx,2)
+for j=1:size(yy,2)
+  zz(i,j) = [1 xx(i) yy(j)]*theta;
+end
+end
+mesh(xx,yy,zz);
+title('Result of Solving Normal Equation');
+legend('Training data', 'Linear regression');
